9ea9774a3024f773cfaec1865cc23dfb0938447d
Mike Hansen <mike@unstablestudios.com>
2025-12-29 10:20:04 -0800

    cleanup fzf styling

--- dot_config/zsh/plugins.d/10-fzf-tab.zsh
+++ dot_config/zsh/plugins.d/10-fzf-tab.zsh
@@ -1,10 +1,24 @@
 PLUGIN_DIR="$HOME/.config/zsh/plugins/fzf-tab"
 
-# fzf-tab needs to be sourced after compinit (you load plugins after compinit, so we're good)
 if [[ -r "$PLUGIN_DIR/fzf-tab.plugin.zsh" ]]; then
   source "$PLUGIN_DIR/fzf-tab.plugin.zsh"
 fi
 
-# Optional: behavior tweaks
+# --- fzf-tab settings ---
+# Don't reorder completion candidates (keeps things predictable)
 zstyle ':fzf-tab:*' sort false
+
+# Let you switch completion groups with , and .
 zstyle ':fzf-tab:*' switch-group ',' '.'
+
+# Previews for directories when completing `cd`
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -la $realpath'
+
+# Preview files/directories for most completions
+zstyle ':fzf-tab:complete:*:*' fzf-preview '
+  if [[ -f $realpath ]]; then
+    (bat --style=numbers --color=always --line-range :200 $realpath 2>/dev/null || sed -n "1,200p" $realpath)
+  else
+    ls -la $realpath
+  fi
+'
